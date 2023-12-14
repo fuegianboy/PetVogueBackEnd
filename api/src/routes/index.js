518bc55aa72d67e3b72ea65d1da0bf627aff88be
fuegianboy <fuegianboy@hotmail.com>
2023-12-14 19:21:44 -0300

    createOrderController

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const usersRoutes = require("./usersRoutes")
+const ordersRoutes = require("./ordersRoutes")
 
 const routes = Router();
 
 routes.use("/users", usersRoutes)
+routes.use("/orders", ordersRoutes)
 
 module.exports = routes;
