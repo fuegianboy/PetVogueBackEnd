dcf3351590254e13775b33df04387d340b365843
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-01-11 18:49:26 -0300

    user update

--- api/src/controllers/users/createUsers.controller.js
+++ api/src/controllers/users/createUsers.controller.js
@@ -35,7 +35,7 @@ const createUser = async (req, res) => {
         if (!created)
             return res.status(404).json({ error: "User with this email or phone already exists." }) */
 
-    const token = jwt.sign({id: newUser.userID}, SECRET_KEY, {expiresIn:"1h"})
+    const token = jwt.sign({id: user.userID}, SECRET_KEY, {expiresIn:"1h"})
     console.log(token, "token")
     
         return res.status(200).json({ user, created, token })
